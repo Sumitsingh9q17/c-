86fc9c4b5db810190d014d0aeae0bdf05bfcc0d5
sumit <ss5603273@gmail.com>
2025-04-02 13:19:39 +0530

    new

--- q111.cpp
+++ q111.cpp
@@ -1,21 +1,33 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
     public:
-        bool isSymmetric(TreeNode* root) {
+      vector<int> subsetSums(vector<int>& arr) {
+          // code here
 
-            if(root == NULL)
-                return true;
-            return isSymmetric(root->left, root->right);
+            vector<int> result;
+            int n = arr.size();
+            int sum = 0;
+            for(int i = 0; i < n; i++)
+            {
+                sum += arr[i];
+            }
+
+            vector<int> dp(sum + 1, 0);
+            dp[0] = 1;
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = sum; j >= arr[i]; j--)
+                {
+                    dp[j] += dp[j - arr[i]];
+                }
+            }
+            for(int i = 0; i <= sum; i++)
+            {
+                if(dp[i] > 0)
+                {
+                    result.push_back(i);
+                }
+            }
+            return result;
             
-        }
-    };
+      }
+  };
